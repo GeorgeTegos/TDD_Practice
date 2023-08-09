efcf253377d01a97c34f38f16422e3abf4f1329f
George Tegos <george.tegos.22@gmail.com>
2023-08-09 17:42:00 +0100

    refactor code for future tests

--- FizzBuzz/fizzBuzz.py
+++ FizzBuzz/fizzBuzz.py
@@ -1,2 +1,23 @@
 def fizz_buzz():
-    pass
+    listToAssert = []
+    for number in range(1,100):
+        if (number % 3 == 0) and (number % 5 == 0):
+            textToPrint = 'FizzBuzz'
+            print(textToPrint)
+            listToAssert.append(textToPrint)
+        elif number % 3 == 0:
+            textToPrint = 'Fizz'
+            print(textToPrint)
+            listToAssert.append(textToPrint)
+        elif number % 5 == 0:
+            textToPrint = 'Buzz'
+            print(textToPrint)
+            listToAssert.append(textToPrint)
+        else:
+            textToPrint = number
+            print(textToPrint)
+            listToAssert.append(textToPrint)
+
+    return listToAssert
+
+fizz_buzz()
--- FizzBuzz/test_fizzBuzz.py
+++ FizzBuzz/test_fizzBuzz.py
@@ -4,25 +4,7 @@ import unittest
 class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz(self):
-        self.listToAssert = []
-        for number in range(1,100):
-            if (number % 3 == 0) and (number % 5 == 0):
-                textToPrint = 'FizzBuzz'
-                print(textToPrint)
-                self.listToAssert.append(textToPrint)
-            elif number % 3 == 0:
-                textToPrint = 'Fizz'
-                print(textToPrint)
-                self.listToAssert.append(textToPrint)
-            elif number % 5 == 0:
-                textToPrint = 'Buzz'
-                print(textToPrint)
-                self.listToAssert.append(textToPrint)
-            else:
-                textToPrint = number
-                print(textToPrint)
-                self.listToAssert.append(textToPrint)
-
+        self.listToAssert = fizz_buzz()
         self.assertEqual('FizzBuzz', self.listToAssert[14])
 
 
