a125cf2ddd1c021d3c27c568a4bfbb907a91e342
George Tegos <george.tegos.22@gmail.com>
2023-08-09 17:30:30 +0100

    setting testing enviroment

---
+++ FizzBuzz/test_fizzBuzz.py
@@ -0,0 +1,12 @@
+from fizzBuzz import fizz_buzz
+import unittest
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_fizz_buzz(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
