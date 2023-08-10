f70b24f74a05c0186c9f548f5e7e96f83520645c
George Tegos <george.tegos.22@gmail.com>
2023-08-10 15:08:29 +0100

    Red test, return string from roman numerals

--- Roman_Numeral/roman_numeral.py
+++ Roman_Numeral/roman_numeral.py
@@ -8,21 +8,24 @@ class RomanNumeral:
             "IX":9,"VIII":8,"VII":7,"VI":6,"V":5,"IV":4,"III":3,"II":2,"I":1
             }
         
-    def find_thousands(self,number):
+
+    def return_string_from_roman_numerals(self,number_to_search):
+        for key,value in self.roman_numerals.items():
+            if value == number_to_search:
+                string_to_return = key
+        return string_to_return
+
+        
+    def return_thousands_of_a_number(self,number):
         string_to_return = ''
         thousands = number // 1000
         
-        return string_to_return + 'M'*thousands
+        return string_to_return + 'M' * thousands
     
-    def find_hundreds(self,number):
-        string_to_return =''
+    def return_hundreds_of_a_number(self,number):
         hundreds = number % 1000
         find_dict_key_to_return = (hundreds // 100) * 100
-
-        for key,value in self.roman_numerals.items():
-            if value == find_dict_key_to_return:
-                string_to_return = key
-
+        string_to_return = self.return_string_from_roman_numerals(find_dict_key_to_return)
         return string_to_return
         
 
--- Roman_Numeral/test_roman_numeral.py
+++ Roman_Numeral/test_roman_numeral.py
@@ -6,12 +6,17 @@ class TestRomanNumeral(unittest.TestCase):
     def setUp(self):
         self.roman_numerals = RomanNumeral()
         self.number = 4423
+        
+
+    def test_return_string_from_roman_numerals(self):
+        self.assertEqual('CD',self.roman_numerals.return_string_from_roman_numerals(900))
 
     def test_find_thousands(self):
-        self.assertEqual('MMMM',self.roman_numerals.find_thousands(self.number))
+        self.assertEqual('MMMM',self.roman_numerals.return_thousands_of_a_number(self.number))
 
     def test_find_hundreds(self):
-        self.assertEqual('CD',self.roman_numerals.find_hundreds(self.number))
+        self.assertEqual('CD',self.roman_numerals.return_hundreds_of_a_number(self.number))
+
 
 
 
