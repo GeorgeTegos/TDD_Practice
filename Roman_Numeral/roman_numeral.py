9e31cb31f37ae977c8c0596acd0c1ebf2c04fc3c
George Tegos <george.tegos.22@gmail.com>
2023-08-10 14:17:57 +0100

    Find thousands Fail test

--- Roman_Numeral/roman_numeral.py
+++ Roman_Numeral/roman_numeral.py
@@ -7,6 +7,13 @@ class RomanNumeral:
             "XC":90,"LXXX":80,"LXX":70,"LX":60,"L":50,"XL":40,"XXX":30,"XX":20,"X":10,
             "IX":9,"VIII":8,"VII":7,"VI":6,"V":5,"IV":4,"III":3,"II":2,"I":1
             }
+        
+    def find_thousands(self,number):
+        string_to_return = ''
+        thousands = number // 1000
+        
+        return string_to_return + 'M'*thousands
+        
 
 
 
--- Roman_Numeral/test_roman_numeral.py
+++ Roman_Numeral/test_roman_numeral.py
@@ -3,4 +3,16 @@ import unittest
 
 class TestRomanNumeral(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        self.roman_numerals = RomanNumeral()
+        self.number = 3423
+
+    def test_find_thousands(self):
+        self.assertEqual(4,self.roman_numerals.find_thousands(self.number))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
