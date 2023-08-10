f3586764b6464eeb018a80d7becf9965aeff391d
George Tegos <george.tegos.22@gmail.com>
2023-08-10 16:17:16 +0100

    Refactor code, dictionary

--- Roman_Numeral/roman_numeral.py
+++ Roman_Numeral/roman_numeral.py
@@ -2,48 +2,47 @@ class RomanNumeral:
 
     def __init__(self):
         self.roman_numerals = {
-            "M":1000,
-            "CM":900,"DCCC":800,"DCC":700,"DC":600,"D":500,"CD":400,"CCC":300,"CC":200,"C":100,
-            "XC":90,"LXXX":80,"LXX":70,"LX":60,"L":50,"XL":40,"XXX":30,"XX":20,"X":10,
-            "IX":9,"VIII":8,"VII":7,"VI":6,"V":5,"IV":4,"III":3,"II":2,"I":1
+            "MMM":3000, "MM":2000, "M":1000,
+            "CM":900, "DCCC":800, "DCC":700, "DC":600, "D":500, "CD":400, "CCC":300, "CC":200, "C":100,
+            "XC":90, "LXXX":80, "LXX":70, "LX":60, "L":50, "XL":40, "XXX":30, "XX":20, "X":10,
+            "IX":9, "VIII":8, "VII":7, "VI":6, "V":5, "IV":4 ,"III":3, "II":2, "I":1
             }
         
 
-    def return_string_from_roman_numerals(self,number_to_search):
-        string_to_return=''
+    def return_string_from_roman_numerals(self, number_to_search):
         for key,value in self.roman_numerals.items():
             if value == number_to_search:
                 string_to_return = key
 
         return string_to_return
         
-    def return_thousands_of_a_number(self,number):
-        string_to_return = ''
-        thousands = number // 1000
-        
-        return string_to_return + 'M' * thousands
+    def return_thousands_of_a_number(self, number):
+        find_dict_key_to_return = (number // 1000) * 1000
+        string_to_return = self.return_string_from_roman_numerals(find_dict_key_to_return)
+
+        return string_to_return
     
-    def return_hundreds_of_a_number(self,number):
+    def return_hundreds_of_a_number(self, number):
         hundreds = number % 1000
         find_dict_key_to_return = (hundreds // 100) * 100
         string_to_return = self.return_string_from_roman_numerals(find_dict_key_to_return)
 
         return string_to_return
         
-    def return_tens_of_a_number(self,number):
+    def return_tens_of_a_number(self, number):
         tens = number % 100
         find_dict_key_to_return = (tens // 10) * 10
         string_to_return = self.return_string_from_roman_numerals(find_dict_key_to_return)
         
         return string_to_return
 
-    def return_units_of_a_number(self,number):
+    def return_units_of_a_number(self, number):
         units = number % 10
         string_to_return = self.return_string_from_roman_numerals(units)
 
         return string_to_return
     
-    def convert_integer_into_roman_numeral(self,number):
+    def convert_integer_into_roman_numeral(self, number):
         roman_numeral_to_return = self.return_thousands_of_a_number(number) 
         roman_numeral_to_return += self.return_hundreds_of_a_number(number)
         roman_numeral_to_return += self.return_tens_of_a_number(number)
@@ -51,14 +50,8 @@ class RomanNumeral:
 
         return roman_numeral_to_return
     
-    def user_feedback(self,number):
+    def user_feedback(self, number):
         converted_number_to_string = self.convert_integer_into_roman_numeral(number)
         message = f"For your number {number} the Roman Numeral convert is {converted_number_to_string}"
         
         return message
-
-
-
-
-
-
--- Roman_Numeral/test_roman_numeral.py
+++ Roman_Numeral/test_roman_numeral.py
@@ -5,29 +5,29 @@ class TestRomanNumeral(unittest.TestCase):
 
     def setUp(self):
         self.roman_numerals = RomanNumeral()
-        self.number = 4423
+        self.number = 3423
         
 
     def test_return_string_from_roman_numerals(self):
-        self.assertEqual('CM',self.roman_numerals.return_string_from_roman_numerals(900))
+        self.assertEqual('CM', self.roman_numerals.return_string_from_roman_numerals(900))
 
     def test_return_thousands_of_a_number(self):
-        self.assertEqual('MMMM',self.roman_numerals.return_thousands_of_a_number(self.number))
+        self.assertEqual('MMM', self.roman_numerals.return_thousands_of_a_number(self.number))
 
     def test_return_hundreds_of_a_number(self):
-        self.assertEqual('CD',self.roman_numerals.return_hundreds_of_a_number(self.number))
+        self.assertEqual('CD', self.roman_numerals.return_hundreds_of_a_number(self.number))
 
     def test_return_tens_of_a_number(self):
-        self.assertEqual('XX',self.roman_numerals.return_tens_of_a_number(self.number))
+        self.assertEqual('XX', self.roman_numerals.return_tens_of_a_number(self.number))
 
     def test_return_units_of_a_number(self):
-        self.assertEqual('III',self.roman_numerals.return_units_of_a_number(self.number))
+        self.assertEqual('III', self.roman_numerals.return_units_of_a_number(self.number))
 
     def test_convert_integer_into_roman_numeral(self):
-        self.assertEqual('MMMMCDXXIII',self.roman_numerals.convert_integer_into_roman_numeral(self.number))
+        self.assertEqual('MMMCDXXIII', self.roman_numerals.convert_integer_into_roman_numeral(self.number))
 
     def test_user_feedback(self):
-        self.assertEqual('For your number 4423 the Roman Numeral convert is MMMMCDXXIII',self.roman_numerals.user_feedback(self.number))
+        self.assertEqual('For your number 3423 the Roman Numeral convert is MMMCDXXIII', self.roman_numerals.user_feedback(self.number))
 
 
 if __name__ == '__main__':
