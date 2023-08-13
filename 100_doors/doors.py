10bd79e0f3ac7f5ff3b6566b88ce9fcfb426b7b1
George Tegos <george.tegos.22@gmail.com>
2023-08-13 13:52:22 +0100

    Setting enviroment for Doors

---
+++ 100_doors/doors.py
@@ -0,0 +1,5 @@
+class Door:
+
+    def __init__(self,door_number,door_status=False):
+        self.door_status = door_status
+        self.door_number = door_number
---
+++ 100_doors/test_doors.py
@@ -0,0 +1,10 @@
+from doors import Door
+import unittest
+
+class TestDoor(unittest.TestCase):
+
+    pass
+
+
+if __name__=='__main__':
+    unittest.main()
