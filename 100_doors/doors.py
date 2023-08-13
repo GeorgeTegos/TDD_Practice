4bf25a5eddb23de4e4d088c5a8d18692f7f3cc1e
George Tegos <george.tegos.22@gmail.com>
2023-08-13 14:08:15 +0100

    Green Test, change door status

--- 100_doors/doors.py
+++ 100_doors/doors.py
@@ -4,5 +4,11 @@ class Door:
         self.door_status = door_status
         self.door_number = door_number
 
-    def change_door_status(self,door):
-        pass
+    def change_door_status(self):
+        if self.door_status == False:
+            self.door_status = True
+            return self.door_status
+        else:
+            self.door_status = False
+            return self.door_status
+        
--- 100_doors/test_doors.py
+++ 100_doors/test_doors.py
@@ -4,10 +4,10 @@ import unittest
 class TestDoor(unittest.TestCase):
 
     def setUp(self):
-        self.door_one = Door(1)
+        self.door_one = Door(1,False)
 
     def test_change_door_status(self):
-        self.assertEqual(True,self.door_one.door_status)
+        self.assertEqual(True,self.door_one.change_door_status())
 
 if __name__=='__main__':
     unittest.main()
