a3813a46c5bce67d51ff39ad8c3947f01e6480d9
George Tegos <george.tegos.22@gmail.com>
2023-08-13 14:02:33 +0100

    Red Test, change door status

--- 100_doors/doors.py
+++ 100_doors/doors.py
@@ -3,3 +3,6 @@ class Door:
     def __init__(self,door_number,door_status=False):
         self.door_status = door_status
         self.door_number = door_number
+
+    def change_door_status(self,door):
+        pass
--- 100_doors/test_doors.py
+++ 100_doors/test_doors.py
@@ -3,8 +3,11 @@ import unittest
 
 class TestDoor(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        self.door_one = Door(1)
 
+    def test_change_door_status(self):
+        self.assertEqual(True,self.door_one.door_status)
 
 if __name__=='__main__':
     unittest.main()
