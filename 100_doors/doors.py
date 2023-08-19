28b3b90cad4f165ab2efc4b2944271f761a0ab8c
George Tegos <george.tegos.22@gmail.com>
2023-08-19 12:18:23 +0100

    change all 100 door object status

--- 100_doors/doors.py
+++ 100_doors/doors.py
@@ -12,6 +12,9 @@ class Doors:
         for door in list_of_door_objects:
             self.add_door_to_doors_list(door)
 
+    def iterate_and_change_door_status(self):
+        for door in self.doors_list:
+            door.change_door_status()
 
     
 
--- 100_doors/test_doors.py
+++ 100_doors/test_doors.py
@@ -24,5 +24,14 @@ class TestDoor(unittest.TestCase):
         self.doors.populate_doors_with_door_object(list_of_door_objects)
         self.assertEqual(100,len(self.doors.doors_list))
 
+    def test_iterate_and_change_door_status(self):
+        list_of_door_objects = create_100_doors()
+        self.doors.populate_doors_with_door_object(list_of_door_objects)
+        self.doors.iterate_and_change_door_status()
+        expect = self.doors.doors_list[0].door_status
+        self.assertEqual(True,expect)
+
+
+
 if __name__=='__main__':
     unittest.main()
